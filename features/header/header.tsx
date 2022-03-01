a44b659b5d5526e80977782fbf78ecac29c86b16
AlexGavalas <alex.gavalas.92@gmail.com>
2022-03-02 00:18:44 +0200

    feat: login button

--- features/header/header.tsx
+++ features/header/header.tsx
@@ -1,10 +1,17 @@
+import { Button } from '../../components/button';
+
 import styles from './header.module.css';
 
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <h1 className={styles.logo}>PLANEAT</h1>
-            <blockquote># Do the thing!</blockquote>
+            <div className={styles.logoWrapper}>
+                <h1 className={styles.logo}>PLANEAT</h1>
+                <blockquote># Do the thing!</blockquote>
+            </div>
+            <div className={styles.button}>
+                <Button>Login</Button>
+            </div>
         </header>
     );
 };
