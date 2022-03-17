787f59bcb940f2e54529ae1fa420038a11d9b843
AlexGavalas <alex.gavalas.92@gmail.com>
2022-03-17 16:34:06 +0200

    feat: moved calendar logic to zustand store

--- features/calendar/calendar.tsx
+++ features/calendar/calendar.tsx
@@ -1,62 +1,21 @@
-import { eachDayOfInterval, endOfWeek, format, startOfWeek } from 'date-fns';
-import { useEffect, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import { Button } from '../../components/button';
-import { ROWS } from './constants';
 import { Content } from './content';
 import { Header } from './header';
 import { useStore } from '../../store';
 
-const getDaysOfWeek = (date: Date) => {
-    return eachDayOfInterval({
-        start: startOfWeek(date, { weekStartsOn: 1 }),
-        end: endOfWeek(date, { weekStartsOn: 1 }),
-    }).map((day) => ({ timestamp: day, label: format(day, 'EEE dd/M') }));
-};
-
-const getInitialState = (currentWeek: Date) => {
-    return Object.fromEntries(
-        ROWS.map((row) =>
-            getDaysOfWeek(currentWeek).map(({ label }) => [
-                `${row}_${label}`,
-                { content: '' },
-            ])
-        ).flat(1)
-    );
-};
-
 export const Calendar = () => {
     const currentWeek = useStore((state) => state.currentWeek);
     const goToNextWeek = useStore((state) => state.nextWeek);
+    const swapDays = useStore((state) => state.swapDays);
+    const editCell = useStore((state) => state.editCell);
     const goToPreviousWeek = useStore((state) => state.previousWeek);
 
-    const [content, setContent] = useState<Record<string, Content>>(() => {
-        return getInitialState(currentWeek);
-    });
-
-    useEffect(() => {
-        setContent(getInitialState(currentWeek));
-    }, [currentWeek]);
-
-    const handleSwap: HandleSwap = ({ destinationId, originId }) => {
-        setContent((prevContent) => {
-            [prevContent[destinationId], prevContent[originId]] = [
-                prevContent[originId],
-                prevContent[destinationId],
-            ];
-
-            return { ...prevContent };
-        });
-    };
-
-    const editCell: EditCell = (key, value) => {
-        setContent((prevContent) => {
-            prevContent[key].content = value;
-            return { ...prevContent };
-        });
-    };
+    const content = useStore(
+        (state) => state.content[currentWeek.toISOString()]
+    );
 
     const onSave = () => {
         // TODO
@@ -72,7 +31,7 @@ export const Calendar = () => {
             <DndProvider backend={HTML5Backend}>
                 <Content
                     content={content}
-                    handleSwap={handleSwap}
+                    handleSwap={swapDays}
                     editCell={editCell}
                 />
             </DndProvider>
--- store.ts
+++ store.ts
@@ -1,18 +1,103 @@
-import { addWeeks, subWeeks } from 'date-fns';
+import {
+    addWeeks,
+    eachDayOfInterval,
+    endOfWeek,
+    format,
+    startOfWeek,
+    subWeeks,
+} from 'date-fns';
+
 import create from 'zustand';
+
+import { ROWS } from './features/calendar/constants';
 
 interface StoreI {
     currentWeek: Date;
+    content: Record<string, Record<string, Content>>;
+    editCell: (key: string, value: string) => void;
+    swapDays: (props: { destinationId: string; originId: string }) => void;
     nextWeek: () => void;
     previousWeek: () => void;
 }
 
+const getDaysOfWeek = (date: Date) => {
+    return eachDayOfInterval({
+        start: startOfWeek(date, { weekStartsOn: 1 }),
+        end: endOfWeek(date, { weekStartsOn: 1 }),
+    }).map((day) => ({ timestamp: day, label: format(day, 'EEE dd/M') }));
+};
+
+const getInitialState = (currentWeek: Date) => {
+    return Object.fromEntries(
+        ROWS.map((row) =>
+            getDaysOfWeek(currentWeek).map(({ label }) => [
+                `${row}_${label}`,
+                { content: '' },
+            ])
+        ).flat(1)
+    );
+};
+
+const NOW = new Date();
+
 export const useStore = create<StoreI>((set) => ({
-    currentWeek: new Date(),
+    currentWeek: NOW,
+    content: {
+        [NOW.toISOString()]: getInitialState(NOW),
+    },
+    editCell: (key, value) =>
+        set((state) => {
+            const prevContent = state.content[state.currentWeek.toISOString()];
+            prevContent[key].content = value;
+            return state;
+        }),
+    swapDays: ({
+        destinationId,
+        originId,
+    }: {
+        destinationId: string;
+        originId: string;
+    }) =>
+        set((state) => {
+            const key = state.currentWeek.toISOString();
+
+            const prevContent = state.content[key];
+
+            [prevContent[destinationId], prevContent[originId]] = [
+                prevContent[originId],
+                prevContent[destinationId],
+            ];
+
+            return {
+                content: {
+                    [key]: { ...prevContent },
+                },
+            };
+        }),
     nextWeek: () =>
-        set((state) => ({
-            currentWeek: addWeeks(state.currentWeek, 1),
-        })),
+        set((state) => {
+            const nextWeek = addWeeks(state.currentWeek, 1);
+            const key = nextWeek.toISOString();
+
+            return {
+                currentWeek: nextWeek,
+                content: {
+                    ...state.content,
+                    [key]: state.content[key] ?? getInitialState(nextWeek),
+                },
+            };
+        }),
     previousWeek: () =>
-        set((state) => ({ currentWeek: subWeeks(state.currentWeek, 1) })),
+        set((state) => {
+            const prevWeek = subWeeks(state.currentWeek, 1);
+            const key = prevWeek.toISOString();
+
+            return {
+                currentWeek: prevWeek,
+                content: {
+                    ...state.content,
+                    [key]: state.content[key] ?? getInitialState(prevWeek),
+                },
+            };
+        }),
 }));
