66ed1cd0b1c3d3b63c1db6d804cb9fcefdaf94f4
AlexGavalas <alex.gavalas.92@gmail.com>
2022-03-27 22:29:57 +0300

    fix: locales path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'gr'],
+        localePath: path.resolve('./public/locales'),
     },
 };
