bc12898897ee221eaf501d64c5f48e664fed6546
AlexGavalas <alex.gavalas.92@gmail.com>
2022-03-27 19:18:28 +0300

    feat: added home illustration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { GetServerSideProps } from 'next';
+import Image from 'next/image';
 import { getUser } from '@supabase/supabase-auth-helpers/nextjs';
-import { Center, Title, Text, List, Group } from '@mantine/core';
+import { Center, Title, Text, List, Group, Box } from '@mantine/core';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const { user } = await getUser(context);
@@ -15,24 +17,38 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
 
     return {
-        props: {},
+        props: {
+            ...(await serverSideTranslations('en', ['common'])),
+        },
     };
 };
 
 const Home = () => {
     return (
-        <div className="home-container">
-            <Center>
-                <Group direction="column" spacing="lg">
-                    <Title order={1}>Your next meal planning solution</Title>
-                    <Text size="lg">You can use it to:</Text>
-                    <List size="lg">
-                        <List.Item>Plan your weekly meals</List.Item>
-                        <List.Item>Collaborate with your nutrionist</List.Item>
-                    </List>
-                </Group>
-            </Center>
-        </div>
+        <Center pt={20}>
+            <Group direction="column" spacing="lg">
+                <Box
+                    style={{
+                        position: 'relative',
+                        margin: 'auto',
+                        width: 500,
+                        height: 500,
+                    }}
+                >
+                    <Image
+                        src="/images/kitchen.png"
+                        layout="fill"
+                        alt="An illustration of a kitchen"
+                    />
+                </Box>
+                <Title order={1}>Your next meal planning solution</Title>
+                <Text size="lg">You can use it to:</Text>
+                <List size="lg">
+                    <List.Item>Plan your weekly meals</List.Item>
+                    <List.Item>Collaborate with your nutrionist</List.Item>
+                </List>
+            </Group>
+        </Center>
     );
 };
 
