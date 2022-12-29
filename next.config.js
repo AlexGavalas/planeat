d5273ef025874148b4ce6ca127b7a554f8aeb6b1
Alex Gavalas <alex.gavalas@workable.com>
2022-12-29 15:50:43 +0200

    fix: removes unknown next config property

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 // @ts-check
 
+const { i18n } = require('./next-i18next.config');
+
 /**
  * @type {import('next').NextConfig}
  **/
-
-const { i18n } = require('./next-i18next.config');
-
 const config = {
     reactStrictMode: true,
     images: {
         domains: ['lh3.googleusercontent.com'],
     },
-    i18n,
+    i18n: {
+        defaultLocale: i18n.defaultLocale,
+        locales: i18n.locales,
+    },
 };
 
 module.exports = config;
