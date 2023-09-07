848827ec6d0a2c52fd60cebf5a73ce99f30c17ee
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-09-08 00:02:38 +0300

    feat: adds ability for the user to define their food preferences

---
+++ src/features/food-preferences/index.ts
@@ -0,0 +1,1 @@
+export { FoodPreferences } from './food-preferences';
---
+++ supabase/migrations/20230907202246_food_preferences.sql
@@ -0,0 +1,2 @@
+alter table public.users add column food_preferences_positive text null;
+alter table public.users add column food_preferences_negative text null;
