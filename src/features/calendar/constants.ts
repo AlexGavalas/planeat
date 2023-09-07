82ca73858ad0f3cb546717ff30e5ce17aa3f79a7
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-09-08 00:33:49 +0300

    feat: marks deleted entry to be removed on save

--- src/features/calendar/constants.ts
+++ src/features/calendar/constants.ts
@@ -1,7 +1,7 @@
 export const ROWS: RowItem[] = [
-    { key: 'morning', label: 'Morning' },
-    { key: 'snack1', label: 'Snack' },
-    { key: 'lunch', label: 'Lunch' },
-    { key: 'snack2', label: 'Snack' },
-    { key: 'dinner', label: 'Dinner' },
+    { key: 'morning' },
+    { key: 'snack1' },
+    { key: 'lunch' },
+    { key: 'snack2' },
+    { key: 'dinner' },
 ];
