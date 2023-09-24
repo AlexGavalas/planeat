7c8b28345e10a0a3ab9896f9953cc47ba45fa4db
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-09-24 13:38:35 +0300

    test: getutcv2

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -1,8 +1,10 @@
 import { atom } from 'jotai';
 
 import { type EditedMeal } from '~types/meal';
-import { getUTCDate } from '~util/date';
+import { getUTCDateV2 } from '~util/date';
 
-export const currentWeekAtom = atom(getUTCDate(new Date()));
+export const currentWeekAtom = atom(
+    getUTCDateV2(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+);
 
 export const unsavedChangesAtom = atom<Record<string, EditedMeal>>({});
