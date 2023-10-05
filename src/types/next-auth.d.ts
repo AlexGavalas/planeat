69f4331534861f9b3f64d72ce5c48884c2e70ffc
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-10-06 00:02:16 +0300

    refactor: allow interfaces in declaration files

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,7 +1,6 @@
 import 'next-auth';
 
 declare module 'next-auth' {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
     interface User {
         locale?: string | null;
     }
