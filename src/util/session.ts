3e139ef7e591655da5c195b6ee6c5d7489a3c59f
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-10-30 01:02:27 +0200

    refactor: updates auth error case

--- src/util/session.ts
+++ src/util/session.ts
@@ -15,12 +15,22 @@ import { getServerSession } from '~api/session';
 import { fetchUser } from '~api/user';
 import { type User } from '~types/user';
 
+const ERROR_MESSAGE = Object.freeze({
+    NO_EMAIL: 'User must have an email',
+    NO_SESSION: 'User must be have a session',
+});
+
+const AUTH_ERROR_MESSAGES = Object.freeze([
+    `Invariant failed: ${ERROR_MESSAGE.NO_SESSION}`,
+    `Invariant failed: ${ERROR_MESSAGE.NO_EMAIL}`,
+]);
+
 export function assertSession(session: unknown): asserts session is Session {
-    invariant(session, 'User must be have a session');
+    invariant(session, ERROR_MESSAGE.NO_SESSION);
 }
 
 export function assertUserEmail(email: unknown): asserts email is string {
-    invariant(email, 'User must have an email');
+    invariant(email, ERROR_MESSAGE.NO_EMAIL);
 }
 
 export type NextApiHandlerWithUser = (params: {
@@ -56,7 +66,7 @@ export const withUser =
                 res.status(400).json({ message: 'Bad Request' });
             } else if (
                 e instanceof Error &&
-                e.message === 'User must be have a session'
+                AUTH_ERROR_MESSAGES.includes(e.message)
             ) {
                 res.status(401).json({ message: 'Unauthorized' });
             } else {
