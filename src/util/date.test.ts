d9f1419df65af745f587ac1246be1ecb2ae9ce92
Alex Gavalas <alex.gavalas.92@gmail.com>
2026-09-09 18:53:40 +0200

    chore: updates node to 24

--- src/util/date.test.ts
+++ src/util/date.test.ts
@@ -13,31 +13,31 @@ describe('getDaysOfWeek', () => {
         expect(days).toStrictEqual([
             {
                 label: 'Mon 28/08/2023',
-                timestamp: new Date('2023-08-27T21:00:00.000Z'),
+                timestamp: new Date('2023-08-28T00:00:00.000Z'),
             },
             {
                 label: 'Tue 29/08/2023',
-                timestamp: new Date('2023-08-28T21:00:00.000Z'),
+                timestamp: new Date('2023-08-29T00:00:00.000Z'),
             },
             {
                 label: 'Wed 30/08/2023',
-                timestamp: new Date('2023-08-29T21:00:00.000Z'),
+                timestamp: new Date('2023-08-30T00:00:00.000Z'),
             },
             {
                 label: 'Thu 31/08/2023',
-                timestamp: new Date('2023-08-30T21:00:00.000Z'),
+                timestamp: new Date('2023-08-31T00:00:00.000Z'),
             },
             {
                 label: 'Fri 01/09/2023',
-                timestamp: new Date('2023-08-31T21:00:00.000Z'),
+                timestamp: new Date('2023-09-01T00:00:00.000Z'),
             },
             {
                 label: 'Sat 02/09/2023',
-                timestamp: new Date('2023-09-01T21:00:00.000Z'),
+                timestamp: new Date('2023-09-02T00:00:00.000Z'),
             },
             {
                 label: 'Sun 03/09/2023',
-                timestamp: new Date('2023-09-02T21:00:00.000Z'),
+                timestamp: new Date('2023-09-03T00:00:00.000Z'),
             },
         ]);
     });
